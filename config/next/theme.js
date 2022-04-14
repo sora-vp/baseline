a8680d4b97b9c93ed49039ecb6b130b5919d24a2
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-04-15 01:59:01 +0700

    mengubah project ke typescript dan persiapan rebuild

--- config/next/theme.js
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from "@chakra-ui/react";
-
-const config = {
-  initialColorMode: "light",
-  useSystemColorMode: false,
-};
-
-const theme = extendTheme({ config });
-export default theme;
--- models/index.js
+++
@@ -1,4 +0,0 @@
-const Paslon = require("./Paslon");
-const User = require("./User");
-
-module.exports = { User, Paslon };
