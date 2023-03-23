28b7fb272b3eb05e51c1fd0199a94e52d2ab007e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-03-23 09:32:21 +0700

    fix: memperbaiki pm2 dan development desktop

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,9 +9,10 @@ module.exports = {
   apps: [
     {
       name: "sora",
-      script: require.resolve("next/dist/bin/next"),
+      script: path.join(appsDir, "sora/node_modules/next/dist/bin/next"),
       env: {
-        NODE_PATH: nodeModulesDir + ":" + appsDir + "/sora/node_modules",
+        NODE_PATH:
+          nodeModulesDir + ":" + path.join(appsDir, "/sora/node_modules"),
       },
       cwd: path.join(root, "apps/sora"),
       args: "start -p 3000",
