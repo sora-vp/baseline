58a630a93d9538997fcbb9abd7623bee2188ec6b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-06-01 10:50:38 +0700

    fix: menambahkan output standalone

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,12 @@
 // Importing env files here to validate on build
+import path from "path";
+import { fileURLToPath } from "url";
+
 import "./src/env.mjs";
 import "@sora/auth/env.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -10,6 +16,11 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+
+  output: "standalone",
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
 };
 
 export default config;
