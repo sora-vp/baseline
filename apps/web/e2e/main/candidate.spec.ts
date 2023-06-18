692016e9f1bc912b713c5cc016c341db4b57bcce
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-06-18 18:00:51 +0700

    feat: menambahkan pengetesan untuk fungsi edit dan hapus

--- apps/web/e2e/main/candidate.spec.ts
+++ apps/web/e2e/main/candidate.spec.ts
@@ -2,15 +2,32 @@ import { expect, test, type Page } from "@playwright/test";
 
 import { prisma } from "@sora/db";
 
+test.describe.configure({ mode: "serial" });
+
+const goToCandidatePage = async (page: Page) => {
+  await page.goto("/kandidat");
+
+  await page
+    .getByRole("button", { name: "Tambah Kandidat Baru", disabled: false })
+    .waitFor();
+  await page.getByRole("paragraph").filter({ hasText: "Kandidat" }).waitFor();
+
+  await expect(
+    page.getByRole("button", { name: "Tambah Kandidat Baru", disabled: false }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("paragraph").filter({ hasText: "Kandidat" }),
+  ).toBeVisible();
+};
+
 test.describe("Add new candidate page testing", () => {
   const goToCandidateAddPage = async (page: Page) => {
-    await page.goto("/kandidat");
+    await goToCandidatePage(page);
 
     await page
-      .getByRole("button", { name: "Tambah Kandidat Baru", disabled: false })
-      .waitFor();
-    await page
-      .getByRole("button", { name: "Tambah Kandidat Baru", disabled: false })
+      .getByRole("button", {
+        name: "Tambah Kandidat Baru",
+      })
       .click();
 
     await page.waitForURL("/kandidat/tambah");
@@ -93,6 +110,136 @@ test.describe("Add new candidate page testing", () => {
     ).toBeVisible();
 
     await page.getByText("Entonk").waitFor();
+
     await expect(page.getByText("Entonk")).toBeVisible();
+    await expect(page.getByText("0 Orang")).toBeVisible();
+    await expect(
+      page.getByRole("img", { name: "Gambar dari kandidat Entonk." }),
+    ).toBeVisible();
+    await expect(page.getByRole("button", { name: "Edit" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Hapus" })).toBeVisible();
   });
+});
+
+test.describe("Edit page functionality testing", () => {
+  test("Candidate name can't be empty", async ({ page }) => {
+    await goToCandidatePage(page);
+
+    // Waiting for Entonk candidate
+    await page.getByText("Entonk").waitFor();
+
+    // Ensure entire information is visible
+    await expect(page.getByText("Entonk")).toBeVisible();
+    await expect(page.getByText("0 Orang")).toBeVisible();
+    await expect(
+      page.getByRole("img", { name: "Gambar dari kandidat Entonk." }),
+    ).toBeVisible();
+    await expect(page.getByRole("button", { name: "Edit" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Hapus" })).toBeVisible();
+
+    // Real test start from here
+    await page.getByRole("button", { name: "Edit" }).click();
+
+    await page.getByPlaceholder("Masukan Nama Kandidat").waitFor();
+    await page.locator('input[type="file"]').waitFor();
+
+    await expect(page.getByPlaceholder("Masukan Nama Kandidat")).toHaveValue(
+      "Entonk",
+    );
+
+    await page.getByPlaceholder("Masukan Nama Kandidat").clear();
+
+    await page.getByRole("button", { name: "Edit" }).click();
+
+    await page.getByText("Diperlukan nama kandidat!").waitFor();
+
+    await expect(page.getByText("Diperlukan nama kandidat!")).toBeVisible();
+  });
+
+  test("Update candidate name to Ujang", async ({ page }) => {
+    await goToCandidatePage(page);
+
+    // Waiting for Entonk candidate
+    await page.getByText("Entonk").waitFor();
+
+    // Ensure entire information is visible
+    await expect(page.getByText("Entonk")).toBeVisible();
+    await expect(page.getByText("0 Orang")).toBeVisible();
+    await expect(
+      page.getByRole("img", { name: "Gambar dari kandidat Entonk." }),
+    ).toBeVisible();
+    await expect(page.getByRole("button", { name: "Edit" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Hapus" })).toBeVisible();
+
+    // Real test start from here, changes Entonk -> Ujang
+    await page.getByRole("button", { name: "Edit" }).click();
+
+    await page.getByPlaceholder("Masukan Nama Kandidat").waitFor();
+    await page.locator('input[type="file"]').waitFor();
+
+    await expect(page.getByPlaceholder("Masukan Nama Kandidat")).toHaveValue(
+      "Entonk",
+    );
+
+    await page.getByPlaceholder("Masukan Nama Kandidat").clear();
+    await page.getByPlaceholder("Masukan Nama Kandidat").type("Ujang");
+
+    await page.getByRole("button", { name: "Edit" }).click();
+
+    await page.waitForURL("/kandidat");
+
+    await page.getByRole("button", { name: "Tambah Kandidat Baru" }).waitFor();
+
+    await expect(
+      page.getByRole("button", { name: "Tambah Kandidat Baru" }),
+    ).toBeVisible();
+
+    await page.getByText("Ujang").waitFor();
+
+    await expect(page.getByText("Ujang")).toBeVisible();
+    await expect(page.getByText("0 Orang")).toBeVisible();
+    await expect(
+      page.getByRole("img", { name: "Gambar dari kandidat Ujang." }),
+    ).toBeVisible();
+    await expect(page.getByRole("button", { name: "Edit" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Hapus" })).toBeVisible();
+  });
+});
+
+test("Delete candidate from available list", async ({ page }) => {
+  await goToCandidatePage(page);
+
+  // Waiting for Ujang candidate
+  await page.getByText("Ujang").waitFor();
+
+  // Ensure entire information is visible
+  await expect(page.getByText("Ujang")).toBeVisible();
+  await expect(page.getByText("0 Orang")).toBeVisible();
+  await expect(
+    page.getByRole("img", { name: "Gambar dari kandidat Ujang." }),
+  ).toBeVisible();
+  await expect(page.getByRole("button", { name: "Edit" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Hapus" })).toBeVisible();
+
+  await page.getByRole("button", { name: "Hapus" }).click();
+
+  await page.getByText("Hapus Kandidat").waitFor();
+
+  await expect(page.getByText("Hapus Kandidat")).toBeVisible();
+  await expect(page.getByRole("button", { name: "Batal" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Hapus" })).toBeVisible();
+
+  await page.getByRole("button", { name: "Hapus" }).click();
+
+  await page
+    .getByText(
+      "Tidak ada kandidat yang tersedia, silahkan tambahkan kandidat terlebih dahulu.",
+    )
+    .waitFor();
+
+  await expect(
+    page.getByText(
+      "Tidak ada kandidat yang tersedia, silahkan tambahkan kandidat terlebih dahulu.",
+    ),
+  ).toBeVisible();
 });
