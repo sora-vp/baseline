af10c01140187e06298355f9ee9063cb7a74c8a5
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-06-11 11:33:02 +0700

    feat: persiapan halaman partisipan

--- apps/web/src/app/(auth)/admin/partisipan/page.tsx
+++ apps/web/src/app/(auth)/admin/partisipan/page.tsx
@@ -1,3 +1,14 @@
 export default function ParticipantPage() {
-  return <></>;
+  return (
+    <div className="h-screen md:h-max">
+      <div className="flex flex-col gap-5 pb-40 md:flex-row">
+        <div className="space-y-1">
+          <h2 className="text-3xl font-semibold tracking-tight">Partisipan</h2>
+          <p className="text-muted-foreground">
+            Kelola partisipan yang tercantum sebagai daftar pemilih tetap.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
