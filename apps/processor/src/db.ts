570af43616460877b778c9df62ce834fd96a3d11
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-07-03 11:16:35 +0700

    fix: memperbaiki tsup yang tidak bisa bundle

---
+++ apps/processor/src/db.ts
@@ -0,0 +1,19 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+
+import * as schema from "@sora-vp/db/schema/main";
+
+import { env } from "./env";
+
+const connectionStr = new URL(`mysql://${env.DB_HOST}/${env.DB_NAME}`);
+connectionStr.username = env.DB_USERNAME;
+connectionStr.password = env.DB_PASSWORD;
+
+const poolConnection = mysql.createPool(connectionStr.toString());
+
+export const db = drizzle(poolConnection, {
+  schema,
+  mode: "default",
+});
+
+export { schema };
