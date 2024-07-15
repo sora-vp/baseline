08fe93603d50b538794aba8e5b3aab7247c265fc
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2024-07-15 10:00:08 +0700

    fix: memperbaiki penempatan posisi log file

--- apps/processor/src/logger.ts
+++ apps/processor/src/logger.ts
@@ -1,29 +1,26 @@
 import path from "path";
-import { fileURLToPath } from "url";
 import pino from "pino";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export const logger = pino({
-  transport: {
-    targets: [
-      {
-        target: "pino-pretty",
-        level: "debug",
-        options: {
-          colorize: true,
-          ignore: "pid,hostname",
-          translateTime: "SYS:standard",
+export const initLogger = (destinationDirectoryPath: string) =>
+  pino({
+    transport: {
+      targets: [
+        {
+          target: "pino-pretty",
+          level: "debug",
+          options: {
+            colorize: true,
+            ignore: "pid,hostname",
+            translateTime: "SYS:standard",
+          },
         },
-      },
-      {
-        target: "pino/file",
-        level: "debug",
-        options: {
-          destination: path.join(__dirname, "..", "processor.log"),
+        {
+          target: "pino/file",
+          level: "debug",
+          options: {
+            destination: path.join(destinationDirectoryPath, "processor.log"),
+          },
         },
-      },
-    ],
-  },
-});
+      ],
+    },
+  });
