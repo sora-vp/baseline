1c061d0b411c78e3f20731e55688232affb65c70
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2025-01-22 22:50:57 +0700

    fix: gimana kalau begini

--- packages/db/src/schema/main.ts
+++ packages/db/src/schema/main.ts
@@ -1,12 +1,4 @@
-import {
-  boolean,
-  int,
-  mysqlEnum,
-  text,
-  timestamp,
-  uniqueIndex,
-  varchar,
-} from "drizzle-orm/mysql-core";
+import { uniqueIndex } from "drizzle-orm/mysql-core";
 
 import { nanoid } from "@sora-vp/id-generator";
 
@@ -14,43 +6,39 @@ import { mySqlTable } from "./_table";
 
 export const users = mySqlTable(
   "user",
-  {
-    id: int("id").autoincrement().primaryKey(),
-    name: text("name").notNull(),
-    email: varchar("email", { length: 255 }).notNull(),
-    password: varchar("password", { length: 255 }).notNull(),
-    verifiedAt: timestamp("verified_at", { mode: "date" }),
-    role: mysqlEnum("role", ["admin", "comittee"]),
-  },
-  (users) => ({
-    emailIndex: uniqueIndex("email_unique_index").on(users.email),
+  (t) => ({
+    id: t.int("id").autoincrement().primaryKey(),
+    name: t.text("name").notNull(),
+    email: t.varchar("email", { length: 255 }).notNull(),
+    password: t.varchar("password", { length: 255 }).notNull(),
+    verifiedAt: t.timestamp("verified_at", { mode: "date" }),
+    role: t.mysqlEnum("role", ["admin", "comittee"]),
   }),
+  (users) => [uniqueIndex("email_unique_index").on(users.email)],
 );
 
-export const candidates = mySqlTable("candidate", {
-  id: int("id").autoincrement().primaryKey(),
-  name: text("name").notNull(),
-  counter: int("counter").notNull().default(0),
-  image: varchar("image", { length: 100 }).notNull(),
-});
+export const candidates = mySqlTable("candidate", (t) => ({
+  id: t.int("id").autoincrement().primaryKey(),
+  name: t.text("name").notNull(),
+  counter: t.int("counter").notNull().default(0),
+  image: t.varchar("image", { length: 100 }).notNull(),
+}));
 
 export const participants = mySqlTable(
   "participant",
-  {
-    id: int("id").autoincrement().primaryKey(),
-    name: text("name").notNull(),
-    subpart: varchar("sub_part", { length: 50 }).notNull(),
-    qrId: varchar("qr_id", { length: 30 }).$defaultFn(() => nanoid()),
+  (t) => ({
+    id: t.int("id").autoincrement().primaryKey(),
+    name: t.text("name").notNull(),
+    subpart: t.varchar("sub_part", { length: 50 }).notNull(),
+    qrId: t.varchar("qr_id", { length: 30 }).$defaultFn(() => nanoid()),
 
     // CRITICAL FEATURE, for presence functionality
-    alreadyAttended: boolean("already_attended").default(false).notNull(),
-    attendedAt: timestamp("attended_at", { mode: "date" }),
+    alreadyAttended: t.boolean("already_attended").default(false).notNull(),
+    attendedAt: t.timestamp("attended_at", { mode: "date" }),
 
     // VERY VERY CRITICAL, for vote functionality
-    alreadyChoosing: boolean("already_choosing").default(false).notNull(),
-    choosingAt: timestamp("choosing_at", { mode: "date" }),
-  },
-  (participants) => ({
-    qrIdIndex: uniqueIndex("qr_id_unique_index").on(participants.qrId),
+    alreadyChoosing: t.boolean("already_choosing").default(false).notNull(),
+    choosingAt: t.timestamp("choosing_at", { mode: "date" }),
   }),
+  (participants) => [uniqueIndex("qr_id_unique_index").on(participants.qrId)],
 );
